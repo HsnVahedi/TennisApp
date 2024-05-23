c33a1f6ca637ef9d0000bb308f595943b7f31dc6
Hossein Vahedi <mcs.hsn.vahedi@gmail.com>
2024-05-23 14:41:33 -0700

    started backend development

---
+++ backend/tennis/artoftennis/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-0ch*ij0_yqdydhmnk=zy(=!g$$io&2+fig581^bkn12pv#zfv$"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
