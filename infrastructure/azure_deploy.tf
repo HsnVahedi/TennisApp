938596a95ccaeda463898089c12ce94c1dc29da6
Hossein Vahedi <mcs.hsn.vahedi@gmail.com>
2024-08-22 16:22:21 -0700

    d

--- infrastructure/azure_deploy.tf
+++ infrastructure/azure_deploy.tf
@@ -36,158 +36,158 @@ module "container_registry" {
 
 
 
-# resource "azurerm_log_analytics_workspace" "backend-log-analytics" {
-#   name                = "backend-log-analytics-${local.safe_prefix}${local.safe_postfix}${var.environment}"
-#   location            = module.resource_group.location
-#   resource_group_name = module.resource_group.name
-#   sku                 = "PerGB2018"
-#   retention_in_days   = 30
-# }
+resource "azurerm_log_analytics_workspace" "backend-log-analytics" {
+  name                = "backend-log-analytics-${local.safe_prefix}${local.safe_postfix}${var.environment}"
+  location            = module.resource_group.location
+  resource_group_name = module.resource_group.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
 
-# resource "azurerm_container_app_environment" "aca-environment" {
-#   name                       = "aca-environment-${local.safe_prefix}${local.safe_postfix}${var.environment}"
-#   location                   = module.resource_group.location
-#   resource_group_name        = module.resource_group.name
-#   log_analytics_workspace_id = azurerm_log_analytics_workspace.backend-log-analytics.id
+resource "azurerm_container_app_environment" "aca-environment" {
+  name                       = "aca-environment-${local.safe_prefix}${local.safe_postfix}${var.environment}"
+  location                   = module.resource_group.location
+  resource_group_name        = module.resource_group.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.backend-log-analytics.id
 
-#   # Removed the unsupported virtual_network block
-# }
+  # Removed the unsupported virtual_network block
+}
 
-# resource "random_password" "db_admin_password" {
-#   length  = 16
-#   special = true
-# }
+resource "random_password" "db_admin_password" {
+  length  = 16
+  special = true
+}
 
-# resource "azurerm_postgresql_server" "db" {
-#   name                = "psql-server-${local.safe_prefix}${local.safe_postfix}${var.environment}"
-#   location            = module.resource_group.location
-#   resource_group_name = module.resource_group.name
+resource "azurerm_postgresql_server" "db" {
+  name                = "psql-server-${local.safe_prefix}${local.safe_postfix}${var.environment}"
+  location            = module.resource_group.location
+  resource_group_name = module.resource_group.name
 
-#   administrator_login          = var.db_admin_username
-#   administrator_login_password = random_password.db_admin_password.result
+  administrator_login          = var.db_admin_username
+  administrator_login_password = random_password.db_admin_password.result
 
-#   version                      = "11"
-#   sku_name                     = "GP_Gen5_2"
-#   storage_mb                   = 5120
-#   backup_retention_days        = 7
-#   geo_redundant_backup_enabled = false
-#   auto_grow_enabled            = true
+  version                      = "11"
+  sku_name                     = "GP_Gen5_2"
+  storage_mb                   = 5120
+  backup_retention_days        = 7
+  geo_redundant_backup_enabled = false
+  auto_grow_enabled            = true
 
-#   public_network_access_enabled = false
-#   ssl_enforcement_enabled       = true  # Add this line
+  public_network_access_enabled = false
+  ssl_enforcement_enabled       = true  # Add this line
 
-#   tags = local.tags
-# }
+  tags = local.tags
+}
 
-# resource "azurerm_postgresql_database" "db" {
-#   name                = "backend_db"
-#   resource_group_name = module.resource_group.name
-#   server_name         = azurerm_postgresql_server.db.name
-#   charset             = "UTF8"
-#   collation           = "English_United States.1252"
-# }
+resource "azurerm_postgresql_database" "db" {
+  name                = "backend_db"
+  resource_group_name = module.resource_group.name
+  server_name         = azurerm_postgresql_server.db.name
+  charset             = "UTF8"
+  collation           = "English_United States.1252"
+}
 
 
-# resource "azurerm_container_app" "backend" {
-#   name                         = "backend-${local.safe_prefix}${local.safe_postfix}${var.environment}"
-#   container_app_environment_id = azurerm_container_app_environment.aca-environment.id
-#   resource_group_name          = module.resource_group.name
-#   revision_mode                = "Single"
+resource "azurerm_container_app" "backend" {
+  name                         = "backend-${local.safe_prefix}${local.safe_postfix}${var.environment}"
+  container_app_environment_id = azurerm_container_app_environment.aca-environment.id
+  resource_group_name          = module.resource_group.name
+  revision_mode                = "Single"
 
-#   template {
-#     container {
-#       name   = "backend"
-#       image  = "${module.container_registry.name}.azurecr.io/backend-${var.branch_name}"
-#       cpu    = 0.25
-#       memory = "0.5Gi"
+  template {
+    container {
+      name   = "backend"
+      image  = "${module.container_registry.name}.azurecr.io/backend-${var.branch_name}"
+      cpu    = 0.25
+      memory = "0.5Gi"
 
-#       env {
-#         name  = "DATABASE_HOST"
-#         value = "${azurerm_postgresql_server.db.name}.privatelink.postgres.database.azure.com"
-#       }
+      env {
+        name  = "DATABASE_HOST"
+        value = "${azurerm_postgresql_server.db.name}.privatelink.postgres.database.azure.com"
+      }
 
-#       env {
-#         name  = "DATABASE_USER"
-#         value = "${var.db_admin_username}@${azurerm_postgresql_server.db.name}"
-#       }
+      env {
+        name  = "DATABASE_USER"
+        value = "${var.db_admin_username}@${azurerm_postgresql_server.db.name}"
+      }
 
-#       env {
-#         name  = "DATABASE_PASSWORD"
-#         value = random_password.db_admin_password.result
-#       }
+      env {
+        name  = "DATABASE_PASSWORD"
+        value = random_password.db_admin_password.result
+      }
 
-#       env {
-#         name  = "DATABASE_NAME"
-#         value = azurerm_postgresql_database.db.name
-#       }
+      env {
+        name  = "DATABASE_NAME"
+        value = azurerm_postgresql_database.db.name
+      }
 
-#       env {
-#         name  = "DATABASE_PORT"
-#         value = "5432"
-#       }
+      env {
+        name  = "DATABASE_PORT"
+        value = "5432"
+      }
 
-#       env {
-#         name  = "PYTHONUNBUFFERED"
-#         value = "1"
-#       }
+      env {
+        name  = "PYTHONUNBUFFERED"
+        value = "1"
+      }
 
-#       env {
-#         name  = "IS_PROD"
-#         value = "True"
-#       }
-#     }
-#   }
+      env {
+        name  = "IS_PROD"
+        value = "True"
+      }
+    }
+  }
 
-#   secret {
-#     name  = "container-registry-password"
-#     value = module.container_registry.admin_password
-#   }
+  secret {
+    name  = "container-registry-password"
+    value = module.container_registry.admin_password
+  }
 
-#   registry {
-#     server   = "${module.container_registry.name}.azurecr.io"
-#     username = module.container_registry.admin_username
-#     password_secret_name = "container-registry-password"
-#   }
+  registry {
+    server   = "${module.container_registry.name}.azurecr.io"
+    username = module.container_registry.admin_username
+    password_secret_name = "container-registry-password"
+  }
 
-#   tags = local.tags
-# }
+  tags = local.tags
+}
 
-# resource "azurerm_virtual_network" "vnet" {
-#   name                = "vnet-${local.safe_prefix}${local.safe_postfix}${var.environment}"
-#   location            = module.resource_group.location
-#   resource_group_name = module.resource_group.name
-#   address_space       = ["10.0.0.0/16"]
-# }
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-${local.safe_prefix}${local.safe_postfix}${var.environment}"
+  location            = module.resource_group.location
+  resource_group_name = module.resource_group.name
+  address_space       = ["10.0.0.0/16"]
+}
 
-# resource "azurerm_subnet" "subnet" {
-#   name                 = "subnet-${local.safe_prefix}${local.safe_postfix}${var.environment}"
-#   resource_group_name  = module.resource_group.name
-#   virtual_network_name = azurerm_virtual_network.vnet.name
-#   address_prefixes     = ["10.0.1.0/24"]
-# }
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-${local.safe_prefix}${local.safe_postfix}${var.environment}"
+  resource_group_name  = module.resource_group.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
 
-# resource "azurerm_private_endpoint" "postgresql_private_endpoint" {
-#   name                = "postgresql-pe-${local.safe_prefix}${local.safe_postfix}${var.environment}"
-#   location            = module.resource_group.location
-#   resource_group_name = module.resource_group.name
-#   subnet_id           = azurerm_subnet.subnet.id
+resource "azurerm_private_endpoint" "postgresql_private_endpoint" {
+  name                = "postgresql-pe-${local.safe_prefix}${local.safe_postfix}${var.environment}"
+  location            = module.resource_group.location
+  resource_group_name = module.resource_group.name
+  subnet_id           = azurerm_subnet.subnet.id
 
-#   private_service_connection {
-#     name                           = "postgresql-sc-${local.safe_prefix}${local.safe_postfix}${var.environment}"
-#     is_manual_connection           = false
-#     private_connection_resource_id = azurerm_postgresql_server.db.id
-#     subresource_names              = ["postgresql"]
-#   }
-# }
+  private_service_connection {
+    name                           = "postgresql-sc-${local.safe_prefix}${local.safe_postfix}${var.environment}"
+    is_manual_connection           = false
+    private_connection_resource_id = azurerm_postgresql_server.db.id
+    subresource_names              = ["postgresql"]
+  }
+}
 
-# resource "azurerm_private_dns_zone" "postgresql_private_dns_zone" {
-#   name                = "privatelink.postgres.database.azure.com"
-#   resource_group_name = module.resource_group.name
-# }
+resource "azurerm_private_dns_zone" "postgresql_private_dns_zone" {
+  name                = "privatelink.postgres.database.azure.com"
+  resource_group_name = module.resource_group.name
+}
 
-# resource "azurerm_private_dns_zone_virtual_network_link" "postgresql_dns_zone_vnet_link" {
-#   name                  = "postgresql-dns-zone-vnet-link-${local.safe_prefix}${local.safe_postfix}${var.environment}"
-#   resource_group_name   = module.resource_group.name
-#   private_dns_zone_name = azurerm_private_dns_zone.postgresql_private_dns_zone.name
-#   virtual_network_id    = azurerm_virtual_network.vnet.id
-# }
+resource "azurerm_private_dns_zone_virtual_network_link" "postgresql_dns_zone_vnet_link" {
+  name                  = "postgresql-dns-zone-vnet-link-${local.safe_prefix}${local.safe_postfix}${var.environment}"
+  resource_group_name   = module.resource_group.name
+  private_dns_zone_name = azurerm_private_dns_zone.postgresql_private_dns_zone.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+}
