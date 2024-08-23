3d6cc9563984c0091f343b54d3cd32b91d16aaaa
Hossein Vahedi <mcs.hsn.vahedi@gmail.com>
2024-08-23 12:34:34 -0700

    d

--- infrastructure/azure_deploy.tf
+++ infrastructure/azure_deploy.tf
@@ -73,69 +73,69 @@ resource "azurerm_container_app_environment" "aca-environment" {
 
 
 
-# resource "azurerm_container_app" "backend" {
-#   name                         = "backend-${local.safe_prefix}${local.safe_postfix}${var.environment}"
-#   container_app_environment_id = azurerm_container_app_environment.aca-environment.id
-#   resource_group_name          = module.resource_group.name
-#   revision_mode                = "Single"
+resource "azurerm_container_app" "backend" {
+  name                         = "backend-${local.safe_prefix}${local.safe_postfix}${var.environment}"
+  container_app_environment_id = azurerm_container_app_environment.aca-environment.id
+  resource_group_name          = module.resource_group.name
+  revision_mode                = "Single"
 
-#   template {
-#     container {
-#       name   = "backend"
-#       image  = "${module.container_registry.name}.azurecr.io/backend-${var.branch_name}@${var.image_digest}"
-#       cpu    = 0.25
-#       memory = "0.5Gi"
+  template {
+    container {
+      name   = "backend"
+      image  = "${module.container_registry.name}.azurecr.io/backend-${var.branch_name}@${var.image_digest}"
+      cpu    = 0.25
+      memory = "0.5Gi"
 
-#       env {
-#         name  = "DATABASE_HOST"
-#         value = azurerm_postgresql_flexible_server.db.fqdn
-#       }
+      env {
+        name  = "DATABASE_HOST"
+        value = azurerm_postgresql_flexible_server.db.fqdn
+      }
 
-#       env {
-#         name  = "DATABASE_USER"
-#         value = "${var.db_admin_username}@${azurerm_postgresql_flexible_server.db.name}"
-#       }
+      env {
+        name  = "DATABASE_USER"
+        value = "${var.db_admin_username}@${azurerm_postgresql_flexible_server.db.name}"
+      }
 
-#       env {
-#         name  = "DATABASE_PASSWORD"
-#         value = random_password.db_admin_password.result
-#       }
+      env {
+        name  = "DATABASE_PASSWORD"
+        value = random_password.db_admin_password.result
+      }
 
-#       env {
-#         name  = "DATABASE_NAME"
-#         value = azurerm_postgresql_flexible_database.db.name
-#       }
+      env {
+        name  = "DATABASE_NAME"
+        value = azurerm_postgresql_flexible_database.db.name
+      }
  
-#       env {
-#         name  = "DATABASE_PORT"
-#         value = "5432"
-#       }
+      env {
+        name  = "DATABASE_PORT"
+        value = "5432"
+      }
 
-#       env {
-#         name  = "PYTHONUNBUFFERED"
-#         value = "1"
-#       }
+      env {
+        name  = "PYTHONUNBUFFERED"
+        value = "1"
+      }
 
-#       env {
-#         name  = "IS_PROD"
-#         value = "True"
-#       }
-#     }
-#   }
+      env {
+        name  = "IS_PROD"
+        value = "True"
+      }
+    }
+  }
 
-#   secret {
-#     name  = "container-registry-password"
-#     value = module.container_registry.admin_password
-#   }
+  secret {
+    name  = "container-registry-password"
+    value = module.container_registry.admin_password
+  }
 
-#   registry {
-#     server   = "${module.container_registry.name}.azurecr.io"
-#     username = module.container_registry.admin_username
-#     password_secret_name = "container-registry-password"
-#   }
+  registry {
+    server   = "${module.container_registry.name}.azurecr.io"
+    username = module.container_registry.admin_username
+    password_secret_name = "container-registry-password"
+  }
 
-#   tags = local.tags
-# }
+  tags = local.tags
+}
 
 
 
