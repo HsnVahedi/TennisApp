74da74f86deff00ad59859f9f732f474a5f20c0c
Hossein Vahedi <mcs.hsn.vahedi@gmail.com>
2024-08-22 17:25:54 -0700

    d

--- backend/tennis/artoftennis/settings/base.py
+++ backend/tennis/artoftennis/settings/base.py
@@ -96,6 +96,18 @@ WSGI_APPLICATION = "artoftennis.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.getenv('DATABASE_NAME'),
+#         "USER": os.getenv('DATABASE_USER'),
+#         "PASSWORD": os.getenv('DATABASE_PASSWORD'),
+#         "HOST": os.getenv('DATABASE_HOST'),
+#         "PORT": os.getenv('DATABASE_PORT', '5432'),
+#     }
+# }
+
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
@@ -104,8 +116,12 @@ DATABASES = {
         "PASSWORD": os.getenv('DATABASE_PASSWORD'),
         "HOST": os.getenv('DATABASE_HOST'),
         "PORT": os.getenv('DATABASE_PORT', '5432'),
+        "OPTIONS": {
+            "sslmode": "require",
+        },
     }
 }
+
 
 
 # Password validation
