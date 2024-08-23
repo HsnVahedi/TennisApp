fb0d3a5c7fa063a5d519fd0a6966b27e72a24082
Hossein Vahedi <mcs.hsn.vahedi@gmail.com>
2024-08-23 12:16:54 -0700

    d

--- infrastructure/azure_deploy.tf
+++ infrastructure/azure_deploy.tf
@@ -1,4 +1,3 @@
-
 locals {
   safe_prefix  = replace(var.prefix, "-", "")
   safe_postfix = replace(var.postfix, "-", "")
@@ -57,7 +56,7 @@ resource "azurerm_postgresql_flexible_server_database" "db" {
   name       = "backend_db"
   server_id  = azurerm_postgresql_flexible_server.db.id
   charset    = "UTF8"
-  collation  = "English_United States.1252"
+  collation  = "en_US.UTF-8"
 }
 
 resource "azurerm_postgresql_flexible_server_firewall_rule" "allow_azure_ips" {
@@ -72,6 +71,7 @@ resource "azurerm_container_app_environment" "aca-environment" {
   location                   = module.resource_group.location
   resource_group_name        = module.resource_group.name
 }
+
 
 
 
