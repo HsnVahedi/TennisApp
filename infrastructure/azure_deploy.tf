c4f5501f8dfbad9f0b7d4f8c4e8008adfa550555
Hossein Vahedi <mcs.hsn.vahedi@gmail.com>
2024-08-22 23:53:58 -0700

    d

--- infrastructure/azure_deploy.tf
+++ infrastructure/azure_deploy.tf
@@ -170,10 +170,19 @@ module "container_registry" {
   tags     = local.tags
 }
 
+
+resource "azurerm_virtual_network" "vitual_network" {
+  name                = "vnet-${local.safe_prefix}${local.safe_postfix}${var.environment}"
+  address_space       = ["10.0.0.0/16"]
+  location            = module.resource_group.location
+  resource_group_name = module.resource_group.name
+}
+
+
 resource "azurerm_subnet" "db" {
   name                 = "db-subnet"
   resource_group_name  = module.resource_group.name
-  virtual_network_name = "vnet-${local.safe_prefix}${local.safe_postfix}${var.environment}"
+  virtual_network_name = azurerm_virtual_network.vitual_network.name
   address_prefixes     = ["10.0.1.0/24"]
   service_endpoints    = ["Microsoft.Storage"]
   delegation {
