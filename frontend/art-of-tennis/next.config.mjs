0ce060a62a5542bd5be63af880e7c4bbb8448a40
Hossein Vahedi <mcs.hsn.vahedi@gmail.com>
2024-09-11 12:11:34 -0700

    d

--- frontend/art-of-tennis/next.config.mjs
+++ frontend/art-of-tennis/next.config.mjs
@@ -2,7 +2,8 @@
 const nextConfig = {
   env: {
     NEXT_PUBLIC_CONTAINER_APP_HOST_URL: `${process.env.CONTAINER_APP_NAME}.${process.env.CONTAINER_APP_ENV_DNS_SUFFIX}`,
-    NEXTAUTH_URL: process.env.IS_PROD ? `https://${process.env.CONTAINER_APP_NAME}.${process.env.CONTAINER_APP_ENV_DNS_SUFFIX}` : 'http://localhost:3000',
+    // NEXTAUTH_URL: process.env.IS_PROD ? `https://${process.env.CONTAINER_APP_NAME}.${process.env.CONTAINER_APP_ENV_DNS_SUFFIX}` : 'http://localhost:3000',
+    NEXTAUTH_URL: process.env.IS_PROD ? `http://artoftennis.ai` : 'http://localhost:3000',
   },
 };
 
